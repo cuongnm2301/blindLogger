f15fb824d75ce61122b63dbad585db408b51f3eb
cuong.nguyen6 <cuong.nguyen6@mservice.com.vn>
2022-05-04 09:40:09 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const { networkInterfaces } = require("os");
+const nets = networkInterfaces();
+let host = "localhost";
+for (const net of nets.en0) {
+  if (net.family === "IPv4" && !net.internal) {
+    host = net.address;
+    break;
+  }
+}
+const PORT = 3000;
+
+io.on("connection", (socket) => {
+  socket.on("disconnect", () => {
+    console.log("logger disconnected");
+  });
+});
+
+app.use(express.static("public"));
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.post("/log", (req, res) => {
+  const time = req.body;
+  const value = req.body;
+  io.emit("ota-log", req.body);
+  res.status(200);
+  res.send();
+});
+
+server.listen(PORT, () => {
+  console.log(`listening on ${host}:${PORT}`);
+});
---
+++ public/log.js
@@ -0,0 +1,43 @@
+const selector = {
+  success: "#4AA36A",
+  info: "#3797EF",
+  warning: "#ff8f00",
+  error: "#ff002b",
+  tracking: "#2C5545",
+};
+
+const LogLevel = {
+  Success: "success",
+  Info: "info",
+  Warning: "warning",
+  Error: "error",
+};
+
+const log = (tag, logLevel, ...message) => {
+  console.log(
+    "%c" + tag,
+    `background-color:${selector[logLevel]};color:white;padding:2px`,
+    ...message
+  );
+};
+
+const Logger = {
+  logInfo: (tag, ...message) => {
+    log(tag, LogLevel.Info, ...message);
+  },
+  logSuccess: (tag, ...message) => {
+    log(tag, LogLevel.Success, ...message);
+  },
+  logWarning: (tag, ...message) => {
+    log(tag, LogLevel.Warning, ...message);
+  },
+  logError: (tag, ...message) => {
+    log(tag, LogLevel.Error, ...message);
+  },
+  logCatchError: (e) => {
+    log("ERROR", LogLevel.Error, e.message || e);
+  },
+  logTracking: (tag, ...message) => {
+    log(tag, "tracking", ...message);
+  },
+};
